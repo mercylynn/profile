eaeabd84b8f52aa07400b8f275f19c61a8775c53
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-23 19:04:00 +0300

    pass props

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 function LinkButton({ name, id, link }) {
     return (
         <div>
-            <a href={link}><button id={id} >{name}</button></a>
+            <a href={link}><button id={id} className="btn">{name}</button></a>
         </div>
     )
 }
